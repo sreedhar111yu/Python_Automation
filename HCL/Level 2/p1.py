90db00613a322c6e02c509c6e425993a679eb3b4
sreedhar111yu <sreedharv36@gmail.com>
2025-11-20 12:33:46 +0530

    l2 -p1

---
+++ HCL/Level 2/p1.py
@@ -0,0 +1,23 @@
+def main():
+    
+    for i in range( 1,6):
+        print(i)
+
+main()
+# 1
+# 2
+# 3
+# 4
+# 5
+
+def loop():
+    x = int(input("Input :"))
+    for i in range (1,x+1):
+        print(i)
+loop()
+# Input :5
+# 1
+# 2
+# 3
+# 4
+# 5
