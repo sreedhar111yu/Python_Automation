2652edee82cee96da5c6bd32b601cae4ed2f498c
sreedhar111yu <sreedharv36@gmail.com>
2025-12-03 11:12:02 +0530

    n

--- HCL/Level 4/p55.py
+++ HCL/Level 4/p55.py
@@ -1,33 +1,41 @@
-def addition(a,b):
-    pass
+def addition(a, b):
+    return a + b
 
-def subtraction(a,b):
-    pass
+def subtraction(a, b):
+    return a - b
 
-def multipication(a,b):
-    pass
+def multiplication(a, b):
+    return a * b
 
-def division(a,b):
-    pass
+def division(a, b):
+    return a // b   # if you want integer division
 
-
-def switch_case(o):
-    switch = {
-        1:addition(),
-        2:subtraction(),
-        3:multipication(),
-        4:division()
-    }
 def main():
-    a = int(input("enter a : "))
-    b = int(input("enter b : "))
-    o = int(input("enter  1 for Add, 2 for Sub,3 for Multi, 4 for Div,5 Exit  enter your choice"))
-    switch_case(o)
-    add = addition(a,b)
-    print(add)
-    sub = subtraction()
-    print(sub)
-    multi = multipication()
-    print(multi)
-    div = division()
-    print(div)
+    while True:
+        print("ENTER THE OPTION FROM BELOW")
+        print("1 - ADD")
+        print("2 - SUB")
+        print("3 - MULTI")
+        print("4 - DIV")
+        print("5 - EXIT")
+
+        option = int(input("Enter option: "))
+
+        if option == 5:
+            break
+
+        a = int(input("Enter a: "))
+        b = int(input("Enter b: "))
+
+        if option == 1:
+            print("Result:", addition(a, b))
+        elif option == 2:
+            print("Result:", subtraction(a, b))
+        elif option == 3:
+            print("Result:", multiplication(a, b))
+        elif option == 4:
+            print("Result:", division(a, b))
+        else:
+            print("Invalid option")
+
+main()
